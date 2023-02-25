0fd896e7b41c2567f5e34313aa4c253f74c4d323
Kamil Chmiel <kamil.chmiel@icloud.com>
2023-02-25 12:29:09 +0100

    init project

---
+++ src/main/java/ch/kchmiel/airpollution/AirPollutionApplication.java
@@ -0,0 +1,13 @@
+package ch.kchmiel.airpollution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirPollutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirPollutionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/kchmiel/airpollution/AirPollutionApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.kchmiel.airpollution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirPollutionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
